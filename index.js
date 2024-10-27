9ab943f4a05b518e4f0afd7972e741144bc7bda9
fuz1700 <fuz77it@gmail.com>
2024-10-27 13:05:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let boyi = 1.85;
+let vesi = 85;
+let bmi = vesi / (boyi*boyi);
+console.log(bmi);
+
+
+let habar = prompt("bilib berishim uchun ballingni yoz!")
+
+if(habar <="70"){
+    alert("kirolmadin!")
+}else if( habar >= '70' && habar <= '80'){
+    alert("Super kontrakt!")
+}else if  (  habar >=80  && habar<=90){
+    alert("Kontrakt!")
+}else if ( habar>90){
+    alert(" grant!")
+
+}
+
+
